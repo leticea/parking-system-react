9428d1258de9590d22612f0411c42345a1824605
leticea <nascimento.leticeah@gmail.com>
2023-02-15 16:50:41 -0300

    inserindo o date

--- src/components/HomeContainer.tsx
+++ src/components/HomeContainer.tsx
@@ -5,7 +5,8 @@ export function HomeContainer() {
   return (
     <div className={styles.container}>
       <form className={styles.formContainer}>
-        <input type="text" placeholder="Digite a placa..." />
+        <input type="text" placeholder="ABC-2124 - João Lucca" name="newCar" />
+        <input type="date" name="time" id="time" />
         <select>
           <option value="search">Pesquisar por</option>
           <option value="plate">Placa</option>
@@ -19,19 +20,19 @@ export function HomeContainer() {
         <header>
           <strong>Placa</strong>
           <p>Nome</p>
-          <time>
-            Entrada
-          </time>
-          <time>
-            Saída
-          </time>
+          <p>Entrada</p>
+          <p>Saída</p>
           <span>Valor</span>
         </header>
         <div className={styles.infoContainer}>
           <strong>ABC-1425</strong>
           <p>Letícia Mangueira</p>
-          <time title="15 de fevereiro de 2023" dateTime="2023-02-15 11:30:02">15/02/23 às 11:30:01</time>
-          <time title="15 de fevereiro de 2023" dateTime="2023-02-15 12:30:01">15/02/23 às 12:30:05</time>
+          <time title="15 de fevereiro de 2023" dateTime="2023-02-15 11:30:02">
+            15/02/23 às 11:30:01
+          </time>
+          <time title="15 de fevereiro de 2023" dateTime="2023-02-15 12:30:01">
+            15/02/23 às 12:30:05
+          </time>
           <span>R$ 30,00</span>
           <button type="submit">
             <CurrencyDollar size={20} />
