fe12ec959d5b88bff3b113c44f5a1eca77ebc187
leticea <nascimento.leticeah@gmail.com>
2023-02-21 18:44:23 -0300

    editando a hora da saída

--- src/components/RegisteredVehicle.tsx
+++ src/components/RegisteredVehicle.tsx
@@ -3,7 +3,7 @@ import { VehiclesProps } from "./HomeContainer";
 import styles from "./RegisteredVehicle.module.css";
 
 import ptBR from "date-fns/locale/pt-BR";
-import { format, setHours } from "date-fns";
+import { format, setHours, setMinutes } from "date-fns";
 import { useState } from "react";
 
 interface VehicleProps {
@@ -34,7 +34,7 @@ export function RegisteredVehicle({ vehicle, removeVehicle }: VehicleProps) {
     ? format(exit, "d 'de' MMMM 'às' HH:mm:ss'hs'", {
         locale: ptBR,
       })
-    : setHours(new Date(), + 1)
+    : new Date().getHours() + 1
 
   const amountFormatted = new Intl.NumberFormat("pt-BR", {
     style: "currency",
@@ -61,7 +61,7 @@ export function RegisteredVehicle({ vehicle, removeVehicle }: VehicleProps) {
           {exitDateFormatted}
         </time>
       ) : (
-        ""
+        new Date().getHours() + 1
       )}
       <span className={isFinished ? styles.finished : styles.inProgress}>
         {amountFormatted}
